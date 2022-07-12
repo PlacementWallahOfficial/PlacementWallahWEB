2da2109936cbeaf4efb146235208d88d1cae0116
placementwallahofficial <placementwallahofficial@gmail.com>
2022-07-12 11:51:49 +0530

    V 0.0.2

--- components/About.jsx
+++ components/About.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import AboutImg from '../public/assets/RajaRaj.jpeg';
+import AboutImg from '../public/assets/RajaRajCrop.png';
 
 const About = () => {
   return (
     <div id='about' className='w-full md:h-screen p-2 flex items-center py-16'>
       <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
         <div className='col-span-2'>
-          <p className='uppercase text-xl tracking-widest text-[#5651e5]'>
+          <p className='uppercase text-2xl tracking-widest text-[#5651e5] drop-shadow-2xl'>
             About
           </p>
-          <h2 className='py-4'>Who I Am</h2>
-          <p className='py-2 text-gray-600'>
+          <h2 className='py-4 drop-shadow-2xl'>Who I Am</h2>
+          {/* <p className='py-2 text-gray-600'>
             / / I am not your average developer
-          </p>
+          </p> */}
           <p className='py-2 text-gray-600'>
              <span className='font-bold text-xl'> Hey Hii I am Raja raj CEO & Founder Of Placement Wallah</span>. I have always had a knack for
             technology and working with computers. In 2018 I started working
@@ -32,11 +32,11 @@ const About = () => {
           </p>
           <Link href='/#projects'>
             <p className='py-2 text-gray-600 underline cursor-pointer'>
-              Check out some of my latest projects.
+              Check out some of my latest Projects.
             </p>
           </Link>
         </div>
-        <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
+        <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-2 hover:scale-105 ease-in duration-300'>
           <Image src={AboutImg} className='rounded-xl' alt='/' />
         </div>
       </div>
