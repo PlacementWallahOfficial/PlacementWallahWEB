c5b1628e241baef66164f6ca4f4c9c8e42c91e85
placementwallahofficial <placementwallahofficial@gmail.com>
2022-07-12 16:57:11 +0530

    V 0.0.2

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,11 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         ></link>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7124168050134229"
+          crossorigin="anonymous"
+        ></script>
       </Head>
       <body>
         <Main />
